76d1312a757c70f010f8a6f5bf60b95f8750dd33
singh2034 <singhaayush2034@gmail.com>
2026-05-29 14:47:43 -0400

    update

--- git-config.sh
+++ git-config.sh
@@ -3,10 +3,10 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install git -y && git -v
 
-git config --global user.name "singh2034" 
-git config --global user.email "singhaayush2034@gmail.com"
+git config --global user.name "username" 
+git config --global user.email "email"
 
-ssh-keygen -t ed25519 -C "singhaayush2034@gmail.com"
+ssh-keygen -t ed25519 -C "email"
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
